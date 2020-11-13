2a5d273626ddf34877ac8c5c44f72a7f08f5de10
Alexandru Mitici <alexandrumitici@gmail.com>
2020-11-13 20:58:40 +0200

    Rewrote html and css to use grid and flexbox and match requirements, started to work on translate.

--- JS/utils.js
+++ JS/utils.js
@@ -1,23 +1,54 @@
 function createArticles(list, type) {
+    var artType = 0;
     var container = document.getElementById('grd');
     var articles = '';
     list.forEach(art => {
-        articles = articles + createArticle(art, type);
+        articles = articles + createArticle(art, type, artType);
+        artType = 1 - artType;
     });
     container.innerHTML = articles;
 }
 
-function createArticle(art, type) {
+function createArticle(art, pageType, artType) {
     var article;
-    if(type == "destination")
-        art.content = art.content + ' <br> <br> <a href="' + art.link + '"  target="_blank" class="link">Click for more info...</a>';
-    else
-        art.title = '<h2>' + art.title + '</h2> <br> <h3>' + art.subtitle + '</h3>';
-    switch(art.layout) {
-        case 1: article = `<article class="art"><img class="artLayout1_Img" src="../img/` + art.image + `"></img><div class="artLayout1_Text"><h2>` + art.title + `</h2> <br>` + art.content + `</div></article>`; break;
-        case 2: article = `<article class="art"><div class="artLayout2_Title"><h2>` + art.title + `</h2> <br></div><img class="artLayout2_Img" src="../img/` + art.image + `"></img><div class="artLayout2_Text">` + art.content + `</div></article>`; break;
-        case 3: article = `<article class="art"><img class="artLayout1_Img" src="../img/` + art.image[0] + `"></img><div class="artLayout1_Text"><h2>` + art.title + `</h2> <br>` + art.content + `</div> <img class="artLayout2_Img_alt" src="../img/` + art.image[1] + `"></img></article>`; break;
-        case 4: article = `<article class="art"><div class="artLayout4_Text"><h2>` + art.title + `</h2> <br>` + art.content + `</div><img class="artLayout4_Img" src="../img/` + art.image + `"></img></article>`; break;
+    if(pageType == "destination") {
+        art.content = art.content + ' <br> <br> <a href="' + art.link + '"  target="_blank" class="link">Click here for more info...</a>';
+        article = `<article class="art art_destination"><div class="art_destination_img"> <button class="imgFullscreenButton" onClick="imageFullscreen(false, '` + art.image + `')"> <img class="artImg" src="../img/` + art.image + `"></img> </button> </div><div class="art_destination_text"><h2>` + art.title + `</h2> <br>` + art.content + `</div></article>`;
+    } else {
+        art.title = '<h2>' + art.title + '</h2> <h3>' + art.subtitle + '</h3>';
+        if(artType == 0)
+            article = `<article class="art art_home0"><div class="art_home_img"> <button class="imgFullscreenButton" onClick="imageFullscreen(false, '` + art.image + `')"> <img class="artImg" src="../img/` + art.image + `"></img> </button> </div><div class="art_home_text_0">` + art.title + art.content + `</div></article>`;
+        else
+            article = `<article class="art art_home1"><div class="art_home_text_1">` + art.title + art.content + `</div><div class="art_home_img"> <button class="imgFullscreenButton" onClick="imageFullscreen(false, '` + art.image + `')"> <img class="artImg" src="../img/` + art.image + `"></img> </button> <div></article>`;
     }
     return article;
+}
+
+function imageFullscreen(close, image) {
+    var elem = document.getElementById("imgFullscreenContainer");
+    if(close == false) {
+        elem.innerHTML = `<button class="imgFullscreenButtonClose" onClick="imageFullscreen(true)"><img class="imgFullscreen" src="../img/` + image + `"></img></button>`;
+        elem.style.display = "block";
+    } else
+        elem.style.display = "none";
+}
+
+function changeLanguage() {
+    if(window.location.search) {
+        // Check if language was changed:
+        const urlParams = new URLSearchParams(window.location.search);
+        const lang = urlParams.get('language');
+        sessionStorage.lang = lang;
+
+        var list = document.getElementsByClassName("languageFormElement");
+        for(let i = 0; i < list.length; i++) {
+            if(list[i].value == lang)
+                list[i].checked = true;
+        }
+    }
+    if(sessionStorage.lang && sessionStorage.lang!="en") {
+        // We need to translate:
+        const lang=sessionStorage.lang;
+        
+    }
 }
