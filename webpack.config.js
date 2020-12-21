be4623c5848905142c5d91fd900edbd2168ec77c
Alexandru Mitici <alexandrumitici@gmail.com>
2020-12-21 17:59:59 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const HtmlPlugins = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: "./JS/main.js",
     devServer: {
         historyApiFallback: true
     },
-    plugins: [new HtmlPlugins({
-        template: "./HTML/home.html"
-    })]
+    plugins: [
+        new HtmlPlugins({
+            template: "./HTML/home.html"
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: './img', to: 'img'
+            }]
+        })
+    ]
 };
