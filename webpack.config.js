311350290568afd7690b6da61a77aed36fa03bc5
Alexandru Mitici <alexandrumitici@gmail.com>
2020-12-21 17:54:38 +0200

    Revert "Update."
    This reverts commit dea35977cb582e6e3167cf6b5befdc0bb06a6dca.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,11 @@
 const HtmlPlugins = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: "./JS/main.js",
     devServer: {
         historyApiFallback: true
     },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: './css', to: 'css'
-                },
-                {
-                    from: './img', to: 'img'
-                }
-            ]
-        }),
-        new HtmlPlugins({
-            template: "./HTML/home.html"
-        })
-    ]
+    plugins: [new HtmlPlugins({
+        template: "./HTML/home.html"
+    })]
 };
